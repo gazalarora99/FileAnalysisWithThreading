a58dec5b2db8862a92a381e1bb45e2df9e3f794e
Charles F. Rodriguez <cfr47@man.cs.rutgers.edu>
2020-11-20 00:09:45 -0500

    Got the thread part of dir_handler to work corretly

--- pc.c
+++ pc.c
@@ -6,7 +6,6 @@
 #include<dirent.h>
 
 void * dir_handler(void * dir_info); 
-
 void * file_handler(void * file_obj);
 
 struct thread_arg{ // this argument stuct is passed to pthread_create so our routine has access to the right arguments 
@@ -42,9 +41,9 @@ void * dir_handler(void * dir_info){
 
 	struct thread_arg * arg = (struct thread_arg *)dir_info ;
 
-	printf("the dir name %s",arg->path);
+	printf("the dir name %s \n",arg->path);
 	DIR * ptr = opendir(arg->path); 
-
+	//printf("isnull %d",ptr == NULL);
 	struct dirent * file  = readdir(ptr); 
 	
 	int safety  = 0; 
@@ -56,21 +55,32 @@ void * dir_handler(void * dir_info){
 
 
 	while(file != NULL){
+		
+		if(file->d_name[0]!='.'){
 			if(file->d_type == DT_DIR){ // checks if current object is a directory
 			
+				//char * old_path  = strcat(arg->path,"");
+				char * old_path = (char *)malloc(sizeof(char)*strlen(arg->path)); 
+				int k=0;  
+
+				for(k;k<strlen(arg->path);k++){ 
+					*(old_path+k) = *(arg->path +k);
+				}
+
+				arg->path = strcat(arg->path,"/"); // gets the name of current dir object and appends it to the path of current obj
+				arg->path = strcat(arg->path,file->d_name);
 				
-				arg->path = strcat(file->d_name, arg->path); // gets the name of current dir object and appends it to the path of current obj
-				
-				pthread_create(&thread, &threadAttr, dir_handler, (void *)(arg) );
-				
+				pthread_create(&thread, &threadAttr, dir_handler, (void *)(arg) );	
+				printf("current path %s\n",old_path);
 				pthread_join(thread, NULL); 
+				arg->path = old_path; // roll back file path after thread has finished exploring previous dir
 				pthread_attr_destroy(&threadAttr);
 			} 
 		else if(file->d_type == DT_REG){ // checks if current object is a file
 				printf("%s\n",file->d_name);
 			
 		 } 
-
+	}
 		loc = telldir(ptr); // gets updated position to move next object
 			
 			
