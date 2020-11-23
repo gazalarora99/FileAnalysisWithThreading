39ffe70bfbb6ca6d7bd290021d0018d5b28dd2e5
Charles F. Rodriguez <cfr47@man.cs.rutgers.edu>
2020-11-23 11:56:50 -0500

    x

--- pc.c
+++ pc.c
@@ -6,18 +6,38 @@
 #include<dirent.h>
 
 void * dir_handler(void * dir_info); 
-void * file_handler(void * file_obj);
+void * file_handler(void * file_info);
+
 
 struct thread_arg{ // this argument stuct is passed to pthread_create so our routine has access to the right arguments 
 	pthread_mutex_t * lock;
 	char * path;	
+}; 
+
+
+
+
+struct Tnode{  // token node for linked list of tokens
+char * token;
+double prob; 
+struct node * next_token;
 };
+
+
+struct Lnode{ // list nodes for list of linked list
+char * file_handle;
+struct Tnode * token_list;
+struct Lnode * next_list;
+};
+
+
+
+
 
 int main(int argc, char * argv[]){ 
 
 
 	char * dir_handle  = argv[1]; 
-
 
 
 	struct thread_arg * dir_obj = (struct thread_arg *)malloc(sizeof(struct thread_arg));  
@@ -32,6 +52,24 @@ int main(int argc, char * argv[]){
 }
 
 
+
+
+
+void * file_handler(void * file_info){ 
+
+
+struct thread_arg *arg  = (struct thread_arg *)file_info;
+
+
+//pthread_mutex_lock(arg->lock);
+printf("a\n");
+// token function call
+// call math functions  
+
+//pthread_mutex_unlock(arg->lock);
+
+pthread_exit(0);
+}
 
 
 
@@ -51,33 +89,98 @@ void * dir_handler(void * dir_info){
 	
 	pthread_attr_t	threadAttr;		
 	pthread_attr_init(&threadAttr); 
-	pthread_t thread;	
+	pthread_t thread;	// handles calls to dir handler 
 
-
+	pthread_t thread2; // handles calls to file handler
+	
+	pthread_mutex_t file_lock;
+	pthread_mutex_init(&file_lock,NULL);
 	while(file != NULL){
 		
 		if(file->d_name[0]!='.'){
 			if(file->d_type == DT_DIR){ // checks if current object is a directory
-			
-				//char * old_path  = strcat(arg->path,"");
-				char * old_path = (char *)malloc(sizeof(char)*strlen(arg->path)); 
+				char * hold  = arg->path;
+				/*
+				int arg_p = strlen(arg->path); 
+				int argFile_d = strlen(file->d_name);
+
+				char * old_path = (char *)malloc(sizeof(char) * (arg_p + argFile_d) ); // malloc space for new concat path
+				*(old_path+arg_p+argFile_d) = '\0';
+				
 				int k=0;  
 
-				for(k;k<strlen(arg->path);k++){ 
+				for(k;k<arg_p;k++){ 
 					*(old_path+k) = *(arg->path +k);
-				}
-
-				arg->path = strcat(arg->path,"/"); // gets the name of current dir object and appends it to the path of current obj
-				arg->path = strcat(arg->path,file->d_name);
+				}  
 				
+				*(old_path + arg_p ) = '/'; 
+				k=0;
+				for(k;k<=argFile_d;k++){ 
+					*(old_path+k+arg_p+1) = *(file->d_name +k);
+				}  
+*/
+				// above two for loops concatenate new address on
+				 /* 
+					decided to do concat myself so that I can keep track of the addresses that I am
+					dynamically allocating
+				 */
+					arg->path = file->d_name;
+				 
 				pthread_create(&thread, &threadAttr, dir_handler, (void *)(arg) );	
-				printf("current path %s\n",old_path);
-				pthread_join(thread, NULL); 
-				arg->path = old_path; // roll back file path after thread has finished exploring previous dir
-				pthread_attr_destroy(&threadAttr);
+				
+//				printf("hold path %s\n",hold);
+
+			//	pthread_join(thread, NULL); 
+				
+			//	free(old_path); // once we have used this new path we free the malloc memory
+				
+				arg->path = hold;// we then resume use of the old path
+				
+				//pthread_attr_destroy(&threadAttr);
 			} 
 		else if(file->d_type == DT_REG){ // checks if current object is a file
-				printf("%s\n",file->d_name);
+			/*	
+				char * hold2  = arg->path;
+				int arg_p2 = strlen(arg->path); 
+				int argFile_d2 = strlen(file->d_name);
+
+				char * old_path2 = (char *)malloc(sizeof(char) * (arg_p2 + argFile_d2) ); // stores original path before updating
+				*(old_path2+arg_p2+argFile_d2) = '\0';
+				int k=0;  
+
+				for(k; k <arg_p2;k++){ 
+					*(old_path2 + k) = *(arg->path +k);
+				}  
+				
+				*(old_path2 + arg_p2 ) = '/'; 
+				
+				k=0;
+				for(k;k<=argFile_d2;k++){ 
+					*(old_path2 +k+arg_p2+1) = *(file->d_name +k);
+				}  
+				printf("current file path %s\n",old_path2);
+			
+				// above two for loops concatenate new address on
+				  
+				//	decided to do concat myself so that I can keep track of the addresses that I am
+				//	dynamically allocating
+				 
+					arg->path = old_path2;
+				 
+//				pthread_create(&thread2, &threadAttr, file_handler, (void *)(arg) );	
+				
+//				printf("hold path %s\n",hold);
+
+	//			pthread_join(thread2, NULL); 
+				
+				free(old_path2); 
+				
+				arg->path = hold2;
+					
+				*/	
+					
+					
+					printf("%s\n",file->d_name);
 			
 		 } 
 	}
@@ -95,9 +198,9 @@ void * dir_handler(void * dir_info){
 	
 	}
 
-
 	closedir(ptr);
-	
+	pthread_attr_destroy(&threadAttr);
+	pthread_join(thread,NULL);
 	pthread_exit(0);
 	
 }
