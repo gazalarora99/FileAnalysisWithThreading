0ce59d4b7c13a9cf690f79f5a48845cb5a7a354a
Charles F. Rodriguez <cfr47@man.cs.rutgers.edu>
2020-11-28 19:30:04 -0500

    issue seems to be fixed

--- working.c
+++ working.c
@@ -341,7 +341,7 @@ while(ptr->next_list!=NULL){
 								
 				mean_list_ptr-> prob = probability_calc( mean_list_ptr->token ,list_ptr, list_ptr2);
 				
-				printf(" %lf \n", mean_list_ptr->prob);
+				printf(" token (%s)  %lf \n",mean_list_ptr->token, mean_list_ptr->prob);
 				mean_list_ptr = mean_list_ptr->next_token;
 				
 			} 
